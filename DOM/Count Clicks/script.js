468a123eafe0869b1757dd6394a01c5310b69857
sa1123 <sahil2398@yahoo.com>
2022-03-27 15:29:19 +0530

    click-count

---
+++ DOM/Count Clicks/script.js
@@ -0,0 +1,10 @@
+var box = document.getElementById('box');
+var clickCount = document.getElementById('click-count');
+
+count = 0;
+
+box.addEventListener('click', function(){
+    count++;
+    console.log(count);
+    clickCount.innerText = count + " ";
+});
