e86da42e2992a2cde00ece70d1a6b023cfc03e4e
sa1123 <sahil2398@yahoo.com>
2022-03-27 15:40:33 +0530

    script

--- Understanding DOM/Count Clicks/script.js
+++ Understanding DOM/Count Clicks/script.js
@@ -1,7 +1,7 @@
 var box = document.getElementById('box');
 var clickCount = document.getElementById('click-count');
 
-count = 0;
+var count = 0;
 
 box.addEventListener('click', function(){
     count++;
