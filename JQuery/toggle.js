e14e4d3284735debf5973c13a41072415c7a7483
sa1123 <sahil2398@yahoo.com>
2022-04-14 23:12:21 +0530

    changed button to toggle and added if else for reversal

--- JQuery/toggle.js
+++ JQuery/toggle.js
@@ -1,4 +1,16 @@
-$("#toggle-button").click(function(){
-    $("body").css("background-color", "black"),
-        $("body h1").css("color", "white");
+var toggled = false;
+var circle = $("#circle");
+
+$("#toggle").click(function(){
+
+    if(!toggled) {
+        circle.css("margin-left", "100px");
+        $("body").css("background-color", "black");
+        $("h1").css("color", "white");
+        toggled = true;
+    } else {
+        circle.css("margin-left", "1px");
+        $("body").css("background-color", "white");
+        $("h1").css("color", "black");
+    }
 });
