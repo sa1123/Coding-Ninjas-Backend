3b4a3c0826078f4b1f7e170e333fbe6c3e01b64a
sa1123 <sahil2398@yahoo.com>
2022-04-18 01:20:21 +0530

    alternative function

--- Ajax/nasa.js
+++ Ajax/nasa.js
@@ -1,15 +1,24 @@
 function displayImage(data){
 
-    $('<img>', {
-        src: data.url,
-        height: '100%',
-        width: '100%',
-        alt: data.explanation
-    }).appendTo('#image-container');
+    // $('<img>', {
+    //     src: data.url,
+    //     height: '100%',
+    //     width: '100%',
+    //     alt: data.explanation
+    // }).appendTo('#image-container');
+
+    // var img = $(document.createElement('img'));
+    // img.attr('src', data.url);
+    // img.appendTo('#image-container');
+
+    $(document.createElement('img')).img.attr('src', data.url).appendTo('#image-container');
 }
 
 $.ajax({
-    url: 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY',
+    url: 'https://api.nasa.gov/planetary/apod',
     method: 'GET',
-    success: displayImage
+    success: displayImage,
+    data: {
+        api_key: 'DEMO_KEY',
+    }
 });
