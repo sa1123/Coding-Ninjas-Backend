f88c6c284a955a50e6cb8c90da01f18228c5c897
sa1123 <sahil2398@yahoo.com>
2022-04-18 00:31:00 +0530

    courses js

--- Ajax/script.js
+++ Ajax/script.js
@@ -1,19 +1,19 @@
 function fetchRandomDogImage(){
 
-    var xhrRequest = new XMLHttpRequest();
+    // var xhrRequest = new XMLHttpRequest();
 
-    xhrRequest.onload = function(){
-        var responseJSON = JSON.parse(xhrRequest.response);
-        var imageUrl = responseJSON.message;
-        $('#dog-image').attr('src', imageUrl);
-    };
+    // xhrRequest.onload = function(){
+    //     var responseJSON = JSON.parse(xhrRequest.response);
+    //     var imageUrl = responseJSON.message;
+    //     $('#dog-image').attr('src', imageUrl);
+    // };
 
-    xhrRequest.onerror = function(){
-        console.log("Request failed");
-    };
+    // xhrRequest.onerror = function(){
+    //     console.log("Request failed");
+    // };
 
-    xhrRequest.open('get', 'https://dog.ceo/api/breeds/image/random', true);
-    xhrRequest.send();
+    // xhrRequest.open('get', 'https://dog.ceo/api/breeds/image/random', true);
+    // xhrRequest.send();
 
     // $.ajax({
     //     url: 'https://dog.ceo/api/breeds/image/random',
