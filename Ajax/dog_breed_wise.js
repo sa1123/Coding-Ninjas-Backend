147aab2bec08df406dd873a579a51e3fcde386e1
sa1123 <sahil2398@yahoo.com>
2022-04-17 19:25:50 +0530

    js, no css

---
+++ Ajax/dog_breed_wise.js
@@ -0,0 +1,48 @@
+var dropdown = $("#breeds-dropdown");
+var breed;
+var allowSubmit = true;
+var dogImage = $("#dog-image")
+
+$.get('https://dog.ceo/api/breeds/list/all', function (data, status) {
+    let breedList = data.message;
+    for (let breed in breedList) {
+        console.log(breed);
+        dropdown.append('<option value="' + breed + '">' + breed + '</option>');
+    }
+    console.log(status)
+});
+
+dropdown.change(function (){
+    allowSubmit = true;
+});
+
+$("form button").click( function (e) {
+    e.preventDefault();
+
+    if(allowSubmit){
+        breed = dropdown.val();
+        displayDog(breed);
+        allowSubmit = false;
+    }
+});
+
+$("#next a").click( function (e) {
+    e.preventDefault();
+
+    if (breed != undefined) {
+        displayDog(breed);
+    }
+});
+
+function displayDog(breed){
+    let url = "https://dog.ceo/api/breed/" + breed + "/images/random";
+
+    $("#dog-image img,h1").remove();
+
+    $.get(url, function (data, status) {
+        let imageUrl = data.message;
+        console.log(data);
+        dogImage.append('<img src="' + imageUrl + '" alt="' + breed + '">');
+        dogImage.append('<h1>' + breed + '</h1>');
+    });
+}
--- Ajax/script.js
+++ Ajax/script.js
@@ -1,33 +1,37 @@
 function fetchRandomDogImage(){
 
-    // var xhrRequest = new XMLHttpRequest();
+    var xhrRequest = new XMLHttpRequest();
 
-    // xhrRequest.onload = function(){
-    //     var responseJSON = JSON.parse(xhrRequest.response);
-    //     var imageUrl = responseJSON.message;
-    //     $('#dog-image').attr('src', imageUrl);
-    // };
+    xhrRequest.onload = function(){
+        var responseJSON = JSON.parse(xhrRequest.response);
+        var imageUrl = responseJSON.message;
+        $('#dog-image').attr('src', imageUrl);
+    };
 
-    // xhrRequest.open('get', 'https://dog.ceo/api/breeds/image/random', true);
-    // xhrRequest.send();
+    xhrRequest.onerror = function(){
+        console.log("Request failed");
+    };
 
-    $.ajax({
-        url: 'https://dog.ceo/api/breeds/image/random',
-        method: 'GET',
-        success: function(data){
-            var imageUrl = data.message;
-            $('#dog-image').attr('src', imageUrl);
-        }
-    }).fail(function(){
-        console.log('error');
-    });
+    xhrRequest.open('get', 'https://dog.ceo/api/breeds/image/random', true);
+    xhrRequest.send();
 
-    // $.get('https://dog.ceo/api/breeds/image/random', function(data){
-    //             var imageUrl = data.message;
-    //             $('#dog-image').attr('src', imageUrl);
-    //         }).fail(function(xhr, textStatus, errorThrown){
-    //             console.log("Request Failed");
-    //         });
+    // $.ajax({
+    //     url: 'https://dog.ceo/api/breeds/image/random',
+    //     method: 'GET',
+    //     success: function(data){
+    //         var imageUrl = data.message;
+    //         $('#dog-image').attr('src', imageUrl);
+    //     }
+    // }).fail(function(){
+    //     console.log('error');
+    // });
+
+    $.get('https://dog.ceo/api/breeds/image/random', function(data){
+                var imageUrl = data.message;
+                $('#dog-image').attr('src', imageUrl);
+            }).fail(function(xhr, textStatus, errorThrown){
+                console.log("Request Failed");
+            });
 }
 
 $('#fetch-dog-image-button').click(fetchRandomDogImage);
