ff91d35a94564b756f583feb9d354c0c92a56e5e
sa1123 <sahil2398@yahoo.com>
2022-04-17 16:52:06 +0530

    error handling

--- Ajax/script.js
+++ Ajax/script.js
@@ -11,19 +11,23 @@ function fetchRandomDogImage(){
     // xhrRequest.open('get', 'https://dog.ceo/api/breeds/image/random', true);
     // xhrRequest.send();
 
-    // $.ajax({
-    //     url: 'https://dog.ceo/api/breeds/image/random',
-    //     method: 'GET',
-    //     success: function(data){
-    //         var imageUrl = data.message;
-    //         $('#dog-image').attr('src', imageUrl);
-    //     }
-    // });
+    $.ajax({
+        url: 'https://dog.ceo/api/breeds/image/random',
+        method: 'GET',
+        success: function(data){
+            var imageUrl = data.message;
+            $('#dog-image').attr('src', imageUrl);
+        }
+    }).fail(function(){
+        console.log('error');
+    });
 
-    $.get('https://dog.ceo/api/breeds/image/random', function(data){
-                var imageUrl = data.message;
-                $('#dog-image').attr('src', imageUrl);
-            });
+    // $.get('https://dog.ceo/api/breeds/image/random', function(data){
+    //             var imageUrl = data.message;
+    //             $('#dog-image').attr('src', imageUrl);
+    //         }).fail(function(xhr, textStatus, errorThrown){
+    //             console.log("Request Failed");
+    //         });
 }
 
 $('#fetch-dog-image-button').click(fetchRandomDogImage);
