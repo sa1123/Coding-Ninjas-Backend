da68d1d7bc4f5a1c3b1270efbc8267986976a3f1
sa1123 <sahil2398@yahoo.com>
2022-04-18 01:03:01 +0530

    nasa pic of day

---
+++ Ajax/nasa.js
@@ -0,0 +1,14 @@
+function displayImage(data){
+
+    $('<img>', {
+        src: data.url,
+        height: '100%',
+        width: '100%'
+    }).appendTo('#image-container');
+}
+
+$.ajax({
+    url: 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY',
+    method: 'GET',
+    success: displayImage
+});
