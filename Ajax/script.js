22abc4c2dfeda36545d670f7e02305d95809a161
sa1123 <sahil2398@yahoo.com>
2022-04-17 16:34:01 +0530

    ajax 1

--- Ajax/script.js
+++ Ajax/script.js
@@ -1,15 +1,24 @@
 function fetchRandomDogImage(){
 
-    var xhrRequest = new XMLHttpRequest();
+    // var xhrRequest = new XMLHttpRequest();
 
-    xhrRequest.onload = function(){
-        var responseJSON = JSON.parse(xhrRequest.response);
-        var imageUrl = responseJSON.message;
-        $('#dog-image').attr('src', imageUrl);
-    };
+    // xhrRequest.onload = function(){
+    //     var responseJSON = JSON.parse(xhrRequest.response);
+    //     var imageUrl = responseJSON.message;
+    //     $('#dog-image').attr('src', imageUrl);
+    // };
 
-    xhrRequest.open('get', 'https://dog.ceo/api/breeds/image/random', true);
-    xhrRequest.send();
-};
+    // xhrRequest.open('get', 'https://dog.ceo/api/breeds/image/random', true);
+    // xhrRequest.send();
+
+    $.ajax({
+        url: 'https://dog.ceo/api/breeds/image/random',
+        method: 'GET',
+        success: function(data){{
+            var imageUrl = data.message;
+            $('#dog-image').attr('src', imageUrl);
+        }}
+    });
+}
 
 $('#fetch-dog-image-button').click(fetchRandomDogImage);
